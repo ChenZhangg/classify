ff8c0a4050b6152a9fb550bd578f2164e7f1c77f
zc <340355960@qq.com>
2018-05-23 12:38:57 +0800

    12:38

--- bin/rsync.rb
+++ bin/rsync.rb
@@ -1,8 +1,12 @@
 #! /usr/bin/ruby
 require 'csv'
-CSV.readlines('java_repo.csv', headers: true).reverse_each do |line|
+file_path = File.expand_path('java_repo.csv', File.dirname(__FILE__))
+
+CSV.readlines(file_path, headers: true).reverse_each do |line|
     repo_name = line[1]
-    source = 'fdse@10.141.221.85:/home/fdse/user/zc/bodyLog2/build_logs/' + repo_name.sub(/\//, '@')
-    dest = '/home/fdse/user/zc/bodyLog2/build_logs/'
+    source = 'fdse@10.141.221.85:~/user/zc/bodyLog2/build_logs/' + repo_name.sub(/\//, '@')
+    dest = File.expand_path(File.join('..', '..', 'bodyLog2', 'build_logs'), File.dirname(__FILE__))
+    p repo_name
     `rsync -avz #{source} #{dest}`
+    p '#{repo_name} over'
 end
