3c6f26a167a5ae20971aaf4b91cb3f268fac0509
zc <340355960@qq.com>
2018-05-23 13:31:15 +0800

    12:38

--- bin/rsync.rb
+++ bin/rsync.rb
@@ -1,12 +1,15 @@
 #! /usr/bin/ruby
 require 'csv'
+require 'open3'
 file_path = File.expand_path('java_repo.csv', File.dirname(__FILE__))
 
 CSV.readlines(file_path, headers: true).reverse_each do |line|
     repo_name = line[1]
     source = 'fdse@10.141.221.85:~/user/zc/bodyLog2/build_logs/' + repo_name.sub(/\//, '@')
     dest = File.expand_path(File.join('..', '..', 'bodyLog2', 'build_logs'), File.dirname(__FILE__))
-    p repo_name
-    `rsync -avz #{source} #{dest}`
-    p '#{repo_name} over'
+    Open3.popen3("rsync -avz #{source} #{dest}") do |i, o, e|
+        while line = o.gets
+            puts line
+        end
+    end
 end
