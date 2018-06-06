2b12ad7574e8f32f2701b5d90fdf8bd3b6aabca7
zc <340355960@qq.com>
2018-06-06 15:08:42 +0800

    14:29

--- Rakefile
+++ Rakefile
@@ -11,10 +11,6 @@ task :property do
   ruby '-Ilib bin/property.rb'
 end
 
-task :log do
-  ruby '-J-Xms4g -J-Xmx4g -Ilib bin/classify.rb'
-end
-
 task :slice do
   ruby '-J-Xms10g -J-Xmx10g -Ilib bin/slice_run.rb'
 end
--- bin/classify.rb
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/ruby
-require 'fdse/parse_log_file'
-
-build_logs_path = File.expand_path(File.join('..', '..', 'bodyLog2', 'build_logs'), File.dirname(__FILE__))
-Fdse::ParseLogFile.scan_log_directory build_logs_path
